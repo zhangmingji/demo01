0b9ff6b6114a77f7f5d76217acc630e8205f2c60
zhangmingji <zhangmingji2007@163.com>
2015-10-18 11:51:47 +0800

    第一次提交

---
+++ src/main/java/com/letv/demo1/entity/User.java
@@ -0,0 +1,34 @@
+package com.letv.demo1.entity;
+
+/**
+ * Created by Administrator on 2015/10/18.
+ */
+public class User {
+    private String username;
+    private String address;
+    private int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
